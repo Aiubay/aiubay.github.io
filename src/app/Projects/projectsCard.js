a91d9f6469489d3375f7b172b5eb9457f7b223f8
aiubay6 <achmad.ubay@erajaya.com>
2024-04-29 19:10:15 +0700

    Fix eslint

--- src/app/Projects/projectsCard.js
+++ src/app/Projects/projectsCard.js
@@ -47,7 +47,7 @@ function ProjectsCards(){
   return (
     <>
       {projects.map((project) => (
-        <div class="relative flex w-full max-w-[26rem] flex-col rounded-xl bg-white bg-clip-border text-gray-700 shadow-lg sm:gap-20 hover:scale-110 dark:bg-gray-800 border-teal-500 border">
+        <div class="relative flex w-full max-w-[26rem] flex-col rounded-xl bg-white bg-clip-border text-gray-700 shadow-lg sm:gap-20 hover:scale-110 dark:bg-gray-800 border-teal-500 border" key={project.id}>
           <div class="relative mx-4 mt-4 overflow-hidden text-white shadow-lg rounded-xl bg-blue-gray-500 bg-clip-border shadow-blue-gray-500/40">
             <img src={project.image} alt="ui/ux review check" />
             {/* <div class="absolute inset-0 w-full h-full to-bg-black-10 bg-gradient-to-tr from-transparent via-transparent to-black/60"></div> */}
